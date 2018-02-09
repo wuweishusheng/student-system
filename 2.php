c7c09384d810fd99f38cdff17177ea828cae594a
zhoushuai <843453123@qq.com>
2018-02-09 21:29:40 +0800

    删除demo

--- 2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$abbreviations = array("AL","AK","AZ","AR"); 
-$states = array("Alabama","Alaska","Arizona","Arkansas"); 
-$stateMap = array_combine($abbreviations,$states);
-echo "<pre>"; 
-print_r($stateMap); 
-?>
